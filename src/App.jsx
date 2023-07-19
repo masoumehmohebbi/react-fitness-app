6cff61296b9e3f86703bd6a3b28ff931ec8e0b9f
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-07-19 21:06:18 +0330

    Added Home Componet

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
 import Layout from './Layout/Layout'
 import routes from './routes'
+import Home from './pages/Home'
 
 function App() {
   return (
     <div className='font-serif bg-[#fdf4ff] dark:bg-slate-800 duration-100 '>
       <Layout>
         <Routes>
+          <Home/>
           {routes.map((route) => (
             <Route {...route} key={route.element}/>
           ))}
