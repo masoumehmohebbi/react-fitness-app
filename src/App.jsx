dec93d9ad423eb00fce3cebe54508f7ea2ee02ad
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-07-19 21:56:14 +0330

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { Route, Routes } from 'react-router-dom'
 import Layout from './Layout/Layout'
 import routes from './routes'
-import Home from './pages/Home'
 
 function App() {
   return (
     <div className='font-serif bg-[#fdf4ff] dark:bg-slate-800 duration-100 '>
       <Layout>
         <Routes>
-          <Home/>
           {routes.map((route) => (
             <Route {...route} key={route.element}/>
           ))}
