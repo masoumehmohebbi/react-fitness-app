6b7d0de7250eea1633634ad2e2933eded83cb8dd
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-07-20 10:13:30 +0330

    Added basename

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,7 +2,7 @@ import Home from './pages/Home'
 import ExerciseDetail from './pages/ExerciseDetail'
 
 const routes = [
-    {path:'/' , element:<Home/>},
+    {path:'/' , element:<Home basename="/react-fitness-app"/>},
     {path:'/exercise/:id' , element:<ExerciseDetail/>}
 ]
 export default routes
