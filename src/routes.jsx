a5749ec1e3c07211ef96dee0690d6bdef33c5fd0
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-07-21 18:41:47 +0330

    Added NotFound Component

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,8 +1,10 @@
 import Home from './pages/Home'
 import ExerciseDetail from './pages/ExerciseDetail'
+import NotFound from './pages/NotFound'
 
 const routes = [
     {path:'/' , element:<Home />},
-    {path:'/exercise/:id' , element:<ExerciseDetail/>}
+    {path:'/exercise/:id' , element:<ExerciseDetail/>},
+    {path:'*', element:<NotFound/>}
 ]
 export default routes
