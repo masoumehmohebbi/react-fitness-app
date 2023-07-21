53d3cd7e9f3db5226ab173cbe841ea49cd5df7f4
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-07-21 18:42:15 +0330

    Removed: Switch

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import { Route, Routes } from 'react-router-dom'
 import Layout from './Layout/Layout'
 import routes from './routes'
 
-
 function App() {
   return (
     <div className='bg-[#fdf4ff] font-serif dark:bg-slate-800 duration-100'>
       <Layout>
-        <Routes>
+        <Routes >
           {routes.map((route) => (
-            <Route {...route} key={route.element}/>
+              <Route {...route} key={route.element}/>
           ))}
         </Routes>
       </Layout>
