51c5fcfeac34553a65e71cd45de066386feb666d
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-07-21 11:40:01 +0330

    Added: Famer-ScrollY

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,9 +6,12 @@ import darkLogo from '../assets/images/dark-logo.png'
 import {BiSun, BiMoon, BiLaptop} from "react-icons/bi";
 import { useEffect, useState } from "react";
 
+import { motion, useScroll } from "framer-motion";
 
 // eslint-disable-next-line react/prop-types
 const NavBar = ({theme , setTheme}) => {
+
+    const { scrollYProgress } = useScroll();
 
       const [open,setOpen]=useState(false);
       const themeOptions = [
@@ -81,37 +84,120 @@ const NavBar = ({theme , setTheme}) => {
     })
     return (
       <nav className='sticky top-0 left-0 w-full z-40'>
+            <motion.div
+            style={{ scaleX: scrollYProgress }}
+            className='
+            fixed top-0 left-0 right-0 h-1 dark:bg-secondary bg-primary origin-left '>
+        
+            </motion.div>
         <div className='md:flex py-3 items-center justify-between bg-[#fdf4ff] dark:bg-slate-800'>
             <div onClick={()=>setOpen(!open)} className='text-slate-800 dark:text-slate-300 text-4xl absolute right-5 cursor-pointer z-50 lg:hidden'>
                 <ion-icon name={open ? 'close':'menu'}></ion-icon>
             </div>
 
             <div className="flex">
-                <Link to="/" className="w-12 h-12 mx-5">
-                    <img className="object-cover" src={armLogo} alt="" />
-                </Link>
+                <motion.div 
+                 whileHover = {{
+                    scale: 1.1,
+                    originX: 0
+                }}
+                transition = {{
+                    type:'spring',
+                    stiffness:'300'
+                }}
+                className="w-12 h-12 mx-5">
+                    <Link to="/" >
+                        <img className="object-cover" src={armLogo} alt="" />
+                    </Link>
+                </motion.div>
 
                 <div className={`flex flex-col gap-y-6 lg:gap-y-0 lg:flex-row lg:items-center 
                 lg:pb-0 pb-12 absolute lg:static bg-[#fdf4ff] lg:bg-transparent lg:z-auto z-[-1]
                 left-0 w-full lg:w-auto lg:pl-0 pl-9 transition-all duration-500 ease-in lg:gap-x-10
                 text-[22px] ${open ? 'top-20 opacity-100':'top-[-490px]'} lg:opacity-100 opacity-0`}>
-
-                    <Link to="/"><span className=" border-b-4 border-primary text-slate-800 dark:text-slate-300">Home</span></Link>
-                    <a href="#exercises" className="text-slate-800 dark:text-slate-300">Exercises</a>
-                    <a href="#classes" className="text-slate-800 dark:text-slate-300">Classes</a>
-                    <a href="#reasons" className="text-slate-800 dark:text-slate-300">Reasons</a>
-                    <a href="#contact-us" className="text-slate-800 dark:text-slate-300">Contact</a>
+                    <motion.div
+                    whileHover = {{
+                        scale: 1.2,
+                        originX: 0
+                    }}
+                    transition = {{
+                        type:'spring',
+                        stiffness:'300'
+                    }}
+                    >
+                        <Link to="/"><span className=" border-b-4 border-primary text-slate-800 dark:text-slate-300">Home</span>
+                        </Link>
+                    </motion.div>
+                    
+                    <motion.a
+                    whileHover = {{
+                        scale: 1.2,
+                        originX: 0
+                        
+                    }}
+                    transition = {{
+                        type:'spring',
+                        stiffness:'300'
+                    }}
+                    href="#exercises" className="text-slate-800 dark:text-slate-300">Exercises</motion.a>
+                    
+                    <motion.a
+                     whileHover = {{
+                        scale: 1.2,
+                        originX: 0 
+                    }}
+                    transition = {{
+                        type:'spring',
+                        stiffness:'300'
+                    }}
+                    href="#classes" className="text-slate-800 dark:text-slate-300">Classes</motion.a>
+                    
+                    <motion.a
+                     whileHover = {{
+                            scale: 1.2,
+                            originX: 0
+                        }}
+                        transition = {{
+                            type:'spring',
+                            stiffness:'300'
+                        }}
+                    href="#reasons" className="text-slate-800 dark:text-slate-300">Reasons</motion.a>
+                    
+                    <motion.a
+                     whileHover = {{
+                        scale: 1.2,
+                        originX: 0
+                    }}
+                    transition = {{
+                        type:'spring',
+                        stiffness:'300'
+                    }}
+                    href="#contact-us" className="text-slate-800 dark:text-slate-300">Contact</motion.a>
                 </div>
             </div>
 
-            <div className="hidden lg:flex gap-x-4 bg-[#fae8ff] dark:bg-slate-400 rounded-md p-2 mr-5">
+            <div 
+            className="hidden lg:flex gap-x-4 bg-[#fae8ff] dark:bg-slate-400 rounded-md p-2 mr-5">
                 {themeOptions.map(opt=>(
                     // eslint-disable-next-line react/jsx-key
-                    <opt.icon 
-                        key={opt.title}
-                        onClick={() => setTheme(opt.title)}
-                        className={`text-2xl cursor-pointer ${theme === opt.title ? 'text-secondary' : 'text-slate-800 '}`}
-                        title={opt.title}/>
+                   <motion.div
+                   key={opt.title}
+                   whileHover = {{
+                    scale: 1.3,
+                    originX: 0
+                        
+                    }}
+                    transition = {{
+                        type:'spring',
+                        stiffness:'300'
+                    }}
+                   >
+                        <opt.icon 
+                            key={opt.title}
+                            onClick={() => setTheme(opt.title)}
+                            className={`text-2xl cursor-pointer ${theme === opt.title ? 'text-secondary' : 'text-slate-800 '}`}
+                            title={opt.title}/>
+                   </motion.div>
                 ))}
             </div>
         </div>
