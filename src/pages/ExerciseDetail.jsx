b8fe3d3083068f4062678d94e652cb1cdd337c94
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-07-25 23:00:54 +0330

    Removed: VideoDetails.jsx

--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -1,29 +1,19 @@
 import { useEffect, useState } from "react";
 import Detail from "../components/Detail";
-import ExerciseVideos from "../components/ExerciseVideos";
 import { useParams } from "react-router-dom";
 import {FetchData, exerciseOptions } from "../../utils/fetchData";
-import { youtubeOptions } from "../../utils/fetchData";
 
 const ExerciseDetail = () => {
     const [exerciseDetail, setExerciseDetail] = useState({})
-    const [exerciseVideos, setExerciseVideos] = useState([])
     const {id} = useParams()
     
     useEffect(()=>{
         const fetchExercusesData = async () =>{
             const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com';
-            const youTubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com';
 
             const exerciseDetailsData = await FetchData(`${exerciseDbUrl}/exercises/exercise/${id}`,
             exerciseOptions);
             setExerciseDetail(exerciseDetailsData.data)
-
-            const exercisesVideosData = await FetchData(
-                `${youTubeSearchUrl}/search?query=${exerciseDetailsData.data.name} exercise`,
-                youtubeOptions
-              );
-            setExerciseVideos(exercisesVideosData.data.contents)
         }
 
         fetchExercusesData()
@@ -32,7 +22,6 @@ const ExerciseDetail = () => {
     return ( 
     <>
         <Detail exerciseDetail = {exerciseDetail}/>
-        <ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name}/>
     </>
     );
 }
--- utils/fetchData.js
+++ utils/fetchData.js
@@ -12,15 +12,6 @@ export const exerciseOptions = {
 
     }  
 };
-export const youtubeOptions = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
-      // 'X-RapidAPI-Key':  '125d34d4fcmsh6cb060fb9462e6ap13d2c6jsnb947a5fda3e9'
-      'X-RapidAPI-Key': '9db52eac7amsh50bf221be8bbe35p195ed1jsnd4ed5fecfe50',
-      // "X-RapidAPI-Key": "f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85",
-    },
-};
 
 export const FetchData = async(url,options) => {
     const response = await axios.get(url, options)
