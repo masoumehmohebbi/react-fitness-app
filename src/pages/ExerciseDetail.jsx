4b062a809ce22744cf715710f22057954dc19645
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-07-25 23:21:18 +0330

    Added UseLocation For ScrollTop

--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -2,8 +2,14 @@ import { useEffect, useState } from "react";
 import Detail from "../components/Detail";
 import { useParams } from "react-router-dom";
 import {FetchData, exerciseOptions } from "../../utils/fetchData";
+import { useLocation } from "react-router";
 
 const ExerciseDetail = () => {
+    const location = useLocation();
+    useEffect(() => {
+      window.scrollTo(0, 0);
+    }, [location]);
+
     const [exerciseDetail, setExerciseDetail] = useState({})
     const {id} = useParams()
     
