ef597450e341264e82bd3834592f21d39a29be6d
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-07-25 20:54:03 +0330

    Add New-Key

--- utils/fetchData.js
+++ utils/fetchData.js
@@ -7,9 +7,8 @@ export const exerciseOptions = {
     //   'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
       'X-RapidAPI-Host': "exercisedb.p.rapidapi.com",
       // 'X-RapidAPI-Key':  '125d34d4fcmsh6cb060fb9462e6ap13d2c6jsnb947a5fda3e9'
-      // 'X-RapidAPI-Key':  'f6f1310788mshc1152382ad06a25p166e3ajsn83b903926726'
-      // 'X-RapidAPI-Key':  ' bd9ec4b8bbmsh477922810441ea4p1c48bbjsn82f93792faeb'
-      "X-RapidAPI-Key": "f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85",
+      'X-RapidAPI-Key': '9db52eac7amsh50bf221be8bbe35p195ed1jsnd4ed5fecfe50',
+      // "X-RapidAPI-Key": "f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85",
 
     }  
 };
@@ -17,8 +16,9 @@ export const youtubeOptions = {
     method: "GET",
     headers: {
       "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
-      // 'X-RapidAPI-Key':  ' bd9ec4b8bbmsh477922810441ea4p1c48bbjsn82f93792faeb'
-      "X-RapidAPI-Key": "f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85",
+      // 'X-RapidAPI-Key':  '125d34d4fcmsh6cb060fb9462e6ap13d2c6jsnb947a5fda3e9'
+      'X-RapidAPI-Key': '9db52eac7amsh50bf221be8bbe35p195ed1jsnd4ed5fecfe50',
+      // "X-RapidAPI-Key": "f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85",
     },
 };
 
